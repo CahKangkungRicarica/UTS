d14dc4efcadfa9e7492a194a288e24b270efd275
CahKangkungRicarica <makarimasafaqig@gmail.com>
2026-09-15 23:10:56 +0700

    memperbaiki delegate

--- Assets/Scripts/Event/BelajarDelegate.cs
+++ Assets/Scripts/Event/BelajarDelegate.cs
@@ -3,9 +3,30 @@ using System;
 
 public class BelajarDelegate : MonoBehaviour
 {
-    
-    public delegate void Kalah(); 
-
+    public delegate void Kalah();
     public delegate void KoinDiambil();
 
+    public Kalah onKalah;
+    public KoinDiambil onKoinDiambil;
+
+    void Start()
+    {
+        onKalah += PesanKalah;
+        onKoinDiambil += PesanKoin;
+
+        onKalah?.Invoke();
+        onKoinDiambil?.Invoke();
+    }
+
+    void PesanKalah()
+    {
+        Debug.Log("Game Over!");
+    }
+
+    void PesanKoin()
+    {
+        Debug.Log("Koin diambil!");
+    }
+    
 }
+
